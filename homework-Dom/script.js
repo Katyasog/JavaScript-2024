9f19beaa608ff1344c7d64cc27371bcee5aa3d3c
katya sog <katuwa6@gmail.com>
2024-03-27 11:13:07 +0200

    initial commit

--- homework-Dom/script.js
+++ homework-Dom/script.js
@@ -4,27 +4,29 @@ const offButton= document.querySelector(".offButton");
 
 
 offButton.addEventListener('click',() => {
-/*document.body.style.backgroundColor="blue";
-offButton.innerText = 'Turn On';? 
-});*/
+
 if (offButton.getAttribute("data-text") == offButton.innerHTML) {
     offButton.innerHTML = offButton.getAttribute("data-text1");
     document.body.style.backgroundColor="brown";
-    const timeStamp = new Date();
-document.getElementById("currentDate").innerText = timeStamp;
+    const lastTurnOff = new Date().toLocaleString("en-GB");
+    localStorage.lastTurnOff = lastTurnOff;
+    const localStorageDateOff = new Date(localStorage.lastTurnOff);
+
+document.getElementById("currentDate").innerText = lastTurnOff;
 } else {
     offButton.setAttribute("data-text1", offButton.innerHTML);
     offButton.innerHTML =offButton.getAttribute("data-text");
     document.body.style.backgroundColor="white";
-    const timeStamp = new Date();
-document.getElementById("currentDate2").innerText = timeStamp;
+    const lastTurnOn = new Date().toLocaleString("en-GB");
+    localStorage.lastTurnOn = lastTurnOn;
+    const localStorageDateOn = new Date(localStorage.lastTurnOn);
+   
+document.getElementById("currentDate2").innerText = lastTurnOn;
    
 }
 },
 false
 );
-//const timeStamp = new Date();
-//document.getElementById("currentDate").innerText = timeStamp;
 
 
     
